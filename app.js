a1539b44e84b6932154ee557e2f734da8e8145a8
thescifer <thescifer@gmail.com>
2019-04-27 11:35:11 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var images = {
+	man: {
+		heading: 'Man Image',
+		description: 'Image of a man.',
+		path: 'img/man.jpg'
+	},
+	wizard: {
+		heading: 'Wizard Image',
+		description: 'Image of a wizard.',
+		path: 'img/wizard.jpg'
+	},
+	beast: {
+		heading: 'Beast Image',
+		description: 'Image of a beast.',
+		path: 'img/beast.jpg'
+	}
+};
